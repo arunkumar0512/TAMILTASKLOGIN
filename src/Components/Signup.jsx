5b4dbca49007f6bfecf9ed8f0053567c901e384c
arunkumar0512 <140463247+arunkumar0512@users.noreply.github.com>
2024-02-04 16:00:53 +0530

    Update Signup.jsx

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -4,9 +4,9 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Signup = () => {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -20,67 +20,69 @@ const Signup = () => {
       })
       .catch((err) => console.log(err));
   };
+
   return (
     <div>
-      <>
-        <div className="d-flex justify-content-center align-items-center bg-secondary vh=100">
-          <div className="bg-white p-3 rounded w-40">
-            <h2>Zen Class Ticketing System for Query Resolving</h2>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="Email">
+      <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+        <div className="bg-white p-3 rounded w-40">
+          <h2>Zen Class Ticketing System for Query Resolving</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="Name">
                 <strong>Name</strong>
               </label>
               <input
                 type="text"
                 placeholder="Enter Name"
                 autoComplete="off"
-                name="Email"
+                name="Name"
                 className="form-control rounded-0"
                 onChange={(e) => setName(e.target.value)}
-              ></input>
-              <div className="mb-3">
-                <label htmlFor="Email">
-                  <strong>Email</strong>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter Email"
-                  autoComplete="off"
-                  name="Email"
-                  className="form-control rounded-0"
-                  onChange={(e) => setEmail(e.target.value)}
-                ></input>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="Password">
-                  <strong>Password</strong>
-                </label>
-                <input
-                  type="Password"
-                  placeholder="Enter Password"
-                  autoComplete="off"
-                  name="Password"
-                  className="form-control rounded-0"
-                  onChange={(e) => setPassword(e.target.value)}
-                ></input>
-              </div>
-              <button type="Submit" className="btn btn-success w-100 rounded-0">
-                Register
-              </button>
-            </form>
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Email">
+                <strong>Email</strong>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter Email"
+                autoComplete="off"
+                name="Email"
+                className="form-control rounded-0"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Password">
+                <strong>Password</strong>
+              </label>
+              <input
+                type="password"
+                placeholder="Enter Password"
+                autoComplete="off"
+                name="Password"
+                className="form-control rounded-0"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit" className="btn btn-success w-100 rounded-0">
+              Register
+            </button>
+          </form>
 
-            <p>Already Have An Account</p>
-            <Link
-              to="/login "
-              className="btn btn-default border w-100 bg-ligh rounded-0 text-decoration-none"
-            >
-              Sign In
-            </Link>
-          </div>
+          <p>Already Have An Account</p>
+          <Link
+            to="/login"
+            className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+          >
+            Sign In
+          </Link>
         </div>
-      </>
+      </div>
     </div>
   );
 };
 
 export default Signup;
+
